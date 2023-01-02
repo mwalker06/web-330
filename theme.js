86ec7dd8fd621b83307fcfd6efecda6b5b42ae90
mwalker06 <81312304+mwalker06@users.noreply.github.com>
2023-01-02 14:26:00 -0600

    fixed links and spacing

---
+++ theme.js
@@ -0,0 +1,30 @@
+/**
+        Title: theme.js
+        Author: Megan Walker
+        Date: 01/01/2023
+        Description: JavaScript to capture, store, and display the selected theme
+        References: WEB 330 GitHub, WEB 330 Assign_1
+ */
+
+
+/**
+ * Set the default theme to light if one has not been set in the browsers localStorage
+ */
+ function setDefaultTheme()
+ {
+     const theme = localStorage.getItem("mode") || "light-theme";
+     const iconMode = localStorage.getItem("iconMode") || "fa-toggle-off";
+     const iconText = localStorage.getItem("iconText") || "Light Mode";
+ 
+     document.body.classList.value = theme;
+     document.getElementById("icon-mode").classList.add(iconMode);
+     document.getElementById("icon-text").innerHTML = iconText;
+ }
+ 
+ /**
+  * Set the HTML body to the user's selected theme. If one has not been selected, set the theme to light-theme
+  */
+ function setSelectedTheme()
+ {
+     document.body.classList.value = localStorage.getItem("mode") || "light-theme";
+ }
